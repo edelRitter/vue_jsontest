c1021f8615cd505f0f19f417427034c96636a514
Isshiki <yohei.isshiki@rakuten.com>
2021-01-20 15:34:35 +0900

    create list from json info

---
+++ src/components/json_data.vue
@@ -0,0 +1,42 @@
+<template>
+  <p class="logo"><img alt="Vue logo" src="/src/assets/logo.png" /></p>
+  <div class="test">
+    <ul>
+      <li v-for="name in names">
+        {{ name }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="pet in pets">
+        {{ pet }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import accounts from "/src/components/name.json";
+export default {
+  name: 'jsontest',
+  computed: {
+    names() {
+      return accounts.accounts.map((info) => {
+        return info.name;
+      })
+    },
+    pets() {
+      return accounts.accounts.map((info) => {
+        return info.pet;
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  position: relative;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
